daa2f879ebf0b4fe9ecd966af1efdb36cbca05d7
Mark Servilla <mark.servilla@gmail.com>
2016-03-15 20:09:23 -0600

    Remove logging configuration and main function.

--- src/settings.py
+++ src/settings.py
@@ -14,22 +14,17 @@
 
 import logging
 
-logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
-                    datefmt='%Y%m%d-%H:%M:%S')
-logging.getLogger('').setLevel(logging.WARN)
-logger = logging.getLogger('settings')
-
-__author__ = "servilla"
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S%z',
+                    filename='/Users/servilla/DataONE/NCEI/ncei.log',
+                    )
 
 
 MN_BASE_URL = 'https://ncei-node.dataone.org/mn'
 CERTIFICATE_FOR_CREATE = '/Users/servilla/Certs/DataONE/urn_node_mnTestNCEI/urn_node_mnTestNCEI.crt'
 CERTIFICATE_FOR_CREATE_KEY = '/Users/servilla/Certs/DataONE/urn_node_mnTestNCEI/private/urn_node_mnTestNCEI.key'
-
-
-def main():
-    return 0
-
-
-if __name__ == "__main__":
-    main()
+CACHE_PATH = '/Users/servilla/DataONE/NCEI/content_cache'
+CACHE_DB = 'cache.sqlite'
+CACHE_REFRESH_FILE = '/Users/servilla/DataONE/NCEI/d1_ncei_adapter/src/cache_refresh.txt'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S%z'
