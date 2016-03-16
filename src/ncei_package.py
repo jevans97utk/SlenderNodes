df0e864f780cf4139ddd7b35c4c8a649b30dd9e7
Mark Servilla <mark.servilla@gmail.com>
2016-03-15 20:02:52 -0600

    Remove logging configuration and add boolean for science metadata only for NODC package iterator.

--- src/ncei_package.py
+++ src/ncei_package.py
@@ -17,12 +17,8 @@ import logging
 
 from nodc_connector import nodc_package
 
-logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
-                    datefmt='%Y%m%d-%H:%M:%S')
-logging.getLogger('').setLevel(logging.WARN)
-logger = logging.getLogger('ncei_package')
 
-__author__ = "servilla"
+logger = logging.getLogger('ncei_package')
 
 
 class Package(object):
@@ -30,7 +26,7 @@ class Package(object):
     def __init__(self, csw_record=None):
         try:
             packager = nodc_package.NODCPackageFTP()
-            self.package = packager.fromIteratorEntry(csw_record)
+            self.package = packager.fromIteratorEntry(csw_record, True)
         except Exception as e:
             logger.error('Failed to load NCEI package - {0}'.format(e.message))
 
