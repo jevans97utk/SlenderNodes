43981dba887cfcc951e5dfaf2ff440dd2feeb2d2
datadavev <datadavev@users.noreply.github.com>
2018-05-16 09:57:43 -0400

    Initial stab at loading json-ld from IEDA

---
+++ schema.org/README.rst
@@ -0,0 +1,58 @@
+Slender Node Adapter Supporting schema.org described Resources
+==============================================================
+
+Notes for implementation of a "slender node" adapter to support synchronization
+of content described with schema.org constructs.
+
+Discovery Pattern
+-----------------
+
+The general pattern for discovery of schema.org resources given a domain name is:
+
+1. parse http(s)://domain.name/robots.txt
+
+   This step is optional if the location of the sitemap.xml is known (step 2)
+
+2. Get the sitemap.xml file from the location identified or inferred from robots.txt
+
+3. For each entry in the sitemap.xml file:
+
+   a. Parse the resource and extract the schema.org information
+
+   b. If sufficient and appropriate schema.org information is available, then add the
+      referenced resource or resources to the set of items to be processed. Note that
+      the sitemap.xml may point to Dataset or a DataCatalog instance. In the latter case,
+      the DataCatalog item should be processed to discover the contained Dataset
+      instances.
+
+A schema.org Dataset instance is considered to be the target item for synchronization by
+DataONE, and the Dataset should contain references to the components of the Dataset.
+
+DataONE treats the Dataset instance as a view of the actual dataset, with parts of the
+view populated from difference components of the dataset such as the metadata and
+resource map or its equivalent providing relationships between components.
+
+
+Dataset Constructs
+------------------
+
+Required properties:
+
+* identifier
+* datePublished
+* dateModified  (required if there are updates after datePublished)
+* distribution
+
+The distribution element must be an array of DataDownload entries, one for each component
+of the dataset being desribed.
+
+Each entry in distribution should include:
+
+* identifier
+* encodingFormat
+* name
+* url
+* "additionalType": "http://www.w3.org/ns/dcat#DataCatalog"
+
+
+
---
+++ schema.org/test_sorg.py
@@ -0,0 +1,150 @@
+'''
+Examine a schema.org resource provider
+
+Script for messing around with schema.org resources, evaluating content exposed by IEDA
+
+Notes:
+
+Resources listed at: http://get.iedadata.org/sitemaps/
+
+'''
+
+import logging
+import sys
+import requests
+from lxml import etree
+from w3lib.html import get_base_url as getBaseUrl
+from extruct.jsonld import JsonLdExtractor
+import json
+import re
+import pprint
+
+NAMESPACES = {"sm":"http://www.sitemaps.org/schemas/sitemap/0.9"}
+
+
+HTML_OR_JS_COMMENTLINE = re.compile('^\s*(//.*|<!--.*-->)')
+
+class ModifiedJsonLdExtractor(JsonLdExtractor):
+  '''
+  Add more diagnostic to the extruct json-ld extractor
+
+  https://github.com/scrapinghub/extruct/blob/master/extruct/jsonld.py
+  '''
+
+  def dumpJsonError(self, script, error):
+    line_no = 1
+    for line in script.split("\n"):
+      print("{}: {}".format(line_no, line))
+      line_no += 1
+    pprint.pprint(error)
+
+
+  def cleanJsonDecodeError(self, script, error):
+    logging.debug("Bad char ordinal = %s at position %d", ord(script[error.pos]), error.pos)
+    cleaned = "{}{}".format(script[:error.pos],script[error.pos+1:])
+    return cleaned
+
+
+  def parseJson(self, script, max_tries = 50):
+    '''
+    Keep trying to parse the provided JSON, replacing bad chars in the text with each try.
+
+    This was needed to parse most of the IEDA content which seems to be malformed.
+
+    :param script: json text
+    :param max_tries: maximum times to try fixing before giving up
+    :return: parsed json or nothing
+    '''
+    n_tries = 0
+    while n_tries < max_tries:
+      try:
+        n_tries += 1
+        logging.debug("Parse json attempt %d", n_tries)
+        data = json.loads(script)
+        if n_tries > 1:
+          logging.warning("Used %d attempts to fix json data!", n_tries)
+        return data
+      except json.decoder.JSONDecodeError as e:
+        if n_tries >= max_tries:
+          self.dumpJsonError(script, e)
+          raise(e)
+        script = self.cleanJsonDecodeError(script, e)
+      except ValueError as e:
+        script = HTML_OR_JS_COMMENTLINE.sub('', script)
+    return None
+
+
+  def _extract_items(self, node):
+    script = node.xpath('string()')
+    data = self.parseJson(script)
+    if isinstance(data, list):
+      return data
+    elif isinstance(data, dict):
+      return [data]
+
+
+def loadResourcesFromSitemap(sitemap):
+  '''
+
+  :param sitemap: URL to sitemap XML document to process
+  :return: list of {url:"", date_modified:""}
+  '''
+  resources = []
+  sitemap_xml = etree.parse(sitemap)
+  urls = sitemap_xml.xpath("//sm:urlset/sm:url", namespaces=NAMESPACES)
+  logging.debug("Found %d entries in sitemap %s", len(urls), sitemap)
+  for url in urls:
+    resource = {
+      "url": url.xpath("sm:loc", namespaces=NAMESPACES)[0].text,
+      "date_modified": url.xpath("sm:lastmod", namespaces=NAMESPACES)[0].text,
+    }
+    resources.append(resource)
+  return resources
+
+
+def loadSchemaOrg(resource):
+  '''
+  Load schema.org information from a specified resource URL
+
+  :param resource: URL
+  :return: dictionary of values
+  '''
+  response = requests.get(resource["url"])
+  data = []
+  extractor = ModifiedJsonLdExtractor()
+  try:
+    data = extractor.extract( response.content )
+  except json.decoder.JSONDecodeError as e:
+    logging.error( e )
+    return {"error": str(e) }
+  if data[0]["@type"] == "Dataset":
+    entry = {
+      "id": data[0]["@id"].strip(),
+      "metadata_url": None,
+      "metadata_format": None,
+    }
+    for ddownload in data[0]["distribution"]:
+      if ddownload["@type"] == "DataDownload":
+        if ddownload["name"] == "ISO Metadata Document":
+          entry["metadata_url"] = ddownload["url"].strip()
+    return entry
+  return None
+
+
+def main():
+  logging.basicConfig(level=logging.INFO)
+  sitemap = "http://get.iedadata.org/sitemaps/usap_sitemap.xml"
+  resources = loadResourcesFromSitemap(sitemap)
+  for resource in resources:
+    sorg_info = loadSchemaOrg(resource)
+    result = {**resource, **sorg_info}
+    try:
+      print("{date_modified}, {url}, {id}, {metadata_url}".format( **result ))
+    except KeyError as e:
+      print("{date_modified}, {url}, {error}".format(**result))
+  return 0
+
+
+if __name__ == "__main__":
+  sys.exit(main())
+
