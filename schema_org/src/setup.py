bce83a8a32548b4022da2c757ad2780f4dd13194
John Evans <jevans97@utk.edu>
2019-08-02 13:40:15 -0400

    Remove python 3.6 from setup.py
    We use asyncio features introduced in 3.7.

--- schema_org/src/setup.py
+++ schema_org/src/setup.py
@@ -42,7 +42,6 @@ kwargs = {
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
     'keywords': (
