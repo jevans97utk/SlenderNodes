b6d8759975e07bcb4e8d8666d92be8f64879f462
John Evans <jevans97@utk.edu>
2019-09-06 09:53:15 -0400

    Remove subclassing of extract_jsonld
    Not sure why we wanted to do this.

--- schema_org/src/schema_org/cuahsi.py
+++ schema_org/src/schema_org/cuahsi.py
@@ -5,12 +5,13 @@ DATAONE adapter for CUAHSI
 # Standard library imports
 
 # Local imports
-from .core import CommonHarvester, SkipError
+from .so_core import SchemaDotOrgHarvester
+from .core import SkipError
 
 SITE_NSMAP = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
 
 
-class CUAHSIHarvester(CommonHarvester):
+class CUAHSIHarvester(SchemaDotOrgHarvester):
 
     def __init__(self, **kwargs):
         super().__init__(id='cuahsi', **kwargs)
@@ -42,3 +43,41 @@ class CUAHSIHarvester(CommonHarvester):
                 f"instead of PUBLISHED."
             )
             raise SkipError(msg)
+
+    def extract_jsonld(self, doc):
+        """
+        Extract JSON-LD from HTML document.
+
+        What we hope for is that:
+            1) jsonld['distribution'][0]['name'] = 'ISO Metadata Document'
+            2) jsonld['distribution'][0]['url'] is the XML url
+            3) jsonld['distribution'][1]['name'] = 'landing page'
+
+        Parameters
+        ----------
+        doc : ElementTree
+            The parsed HTML.  The JSON-LD should be embedded within a
+            <SCRIPT> element embedded in the <HEAD> element.
+        """
+        self.logger.debug('extract_jsonld:')
+        path = './/script[@type="application/ld+json"]'
+        scripts = doc.xpath(path)
+        if len(scripts) == 0:
+            raise SkipError(NO_JSON_LD_SCRIPT_ELEMENTS)
+
+        jsonld = None
+        for script in scripts:
+
+            j = json.loads(script.text)
+            if '@type' in j and j['@type'] == 'Dataset':
+                jsonld = j
+
+        if jsonld is None:
+            msg = (
+                "Could not locate a JSON-LD <SCRIPT> element with @type "
+                "\"Dataset\"."
+            )
+            raise RuntimeError(msg)
+
+        return jsonld
+
--- schema_org/src/tests/test_cuahsi.py
+++ schema_org/src/tests/test_cuahsi.py
@@ -7,8 +7,8 @@ import re
 from aioresponses import aioresponses
 
 # local imports
-from schema_org import D1CheckSitemap
 from schema_org.core import NO_JSON_LD_SCRIPT_ELEMENTS
+from schema_org.cuahsi import CUAHSIHarvester
 from .test_common import TestCommon
 
 
@@ -17,41 +17,4 @@ class TestSuite(TestCommon):
     def setUp(self):
         # Every URL request for ARM will match this pattern, so we set
         # aioresponses to intercept all of them.
-        self.pattern = re.compile(r'https://www.archive.arm.gov/metadata/adc')
-
-    @aioresponses()
-    def test_landing_page_is_missing_jsonld(self, aioresp_mocker):
-        """
-        SCENARIO:  The sitemap references two documents.  The first landing
-        page document does not have JSON-LD.  The 2nd one does have it.
-
-        EXPECTED RESULT:  The log records the JSON-LD failure, but also the
-        successful ingest of the 2nd document.
-        """
-
-        # External calls to read the:
-        #
-        #   1) sitemap
-        #   2) HTML document for record 1 (no XML document can be accessed)
-        #   3) HTML document for record 2
-        #   4) XML document for record 2
-        #
-        contents = [
-            ir.read_text('tests.data.arm', 'sitemap2.xml'),
-            ir.read_text('tests.data.arm', 'nsaqcrad1longC2.c2.no_jsonld.html'),  # noqa: E501
-            ir.read_text('tests.data.arm', 'nsasondewnpnS01.b1.fixed.html'),
-            ir.read_text('tests.data.arm', 'nsanimfraod1michC2.c1.fixed.xml'),
-        ]
-        aioresp_mocker.get(self.pattern, body=contents[0])
-        aioresp_mocker.get(self.pattern, body=contents[1])
-        aioresp_mocker.get(self.pattern, body=contents[2])
-        aioresp_mocker.get(self.pattern, body=contents[3])
-
-        sitemap = 'https://www.archive.arm.gov/metadata/adc/sitemap_not.xml'
-        obj = D1CheckSitemap(sitemap_url=sitemap)
-
-        with self.assertLogs(logger=obj.logger, level='DEBUG') as cm:
-            asyncio.run(obj.run())
-
-            self.assertWarningLogMessage(cm.output, NO_JSON_LD_SCRIPT_ELEMENTS)
-            self.assertSuccessfulDebugIngest(cm.output)
+        self.pattern = re.compile(r'https://www.hydroshare.org/')
